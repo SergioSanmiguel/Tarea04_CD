d49dbc34c28d553773bff9dc235ccf46b88220d3
sergi <sergi@PC-SERGIO>
2024-03-07 17:37:51 +0100

    apartado 1

--- src/tareacd4/CtaCorriente.java
+++ src/tareacd4/CtaCorriente.java
@@ -1,30 +1,30 @@
 package tareacd4;
 
-public class CC {
+public class CtaCorriente {
     public String nombre;
     public String cuenta;
-    public double saldo;
+    public double saldocuenta;
     public double interes;
 
-    public CC()
+    public CtaCorriente()
     {
     }
-    public CC(String nombre, String cuenta, double saldo, double interes)
+    public CtaCorriente(String nombre, String cuenta, double saldo, double interes)
     {
         this.nombre = nombre;
         this.cuenta = cuenta;
-        this.saldo = saldo;
+        this.saldocuenta = saldo;
         this.interes = interes;
     }
     public double estado()
     {
-        return saldo;
+        return saldocuenta;
     }
     public void ingresar(double cantidad) throws Exception
     {
         if (cantidad<0)
             throw new Exception("No se puede ingresar una cantidad negativa");
-        saldo = saldo + cantidad;
+        saldocuenta = saldocuenta + cantidad;
     }
 
     public void retirar(double cantidad) throws Exception
@@ -33,6 +33,6 @@ public class CC {
             throw new Exception ("No se puede retirar una cantidad negativa");
         if (estado()< cantidad)
             throw new Exception ("No se hay suficiente saldo");
-        saldo = saldo - cantidad;
+        saldocuenta = saldocuenta - cantidad;
     }
 }
